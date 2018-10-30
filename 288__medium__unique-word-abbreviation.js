06b04b669d9d6999f63b0c7fc1d0f5bcdbb8c55f
dalinhuang99 <dhuan023@gmail.com>
2018-10-30 01:10:24 -0400

    Oct 30, 2018

---
+++ 288__medium__unique-word-abbreviation.js
@@ -0,0 +1,109 @@
+/**
+ * @param
+ * @return
+ */
+
+/**
+ *
+ * O(n) time preprocess
+ * O(n) space for map
+ * O(1) for isUnique() call
+ *
+ * 45m
+ */
+
+/*
+	=================== solution 1 (115ms) ===================
+*/
+/**
+ * @param {string[]} NOTE: dictionary is unique here!!!
+ */
+var ValidWordAbbr = function(dictionary) {
+    this.map = new Map();
+    for (let i = 0; i < dictionary.length; i++) {
+        let w = dictionary[i];
+        if (w.length < 3) this.map.set(w, [w]);
+        else {
+            let key = this.toAbbr(w);
+            let group = this.map.get(key) || [];
+            group.push(w);
+            this.map.set(key, group);
+        }
+    }
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+ValidWordAbbr.prototype.isUnique = function(word) {
+    let len = word.length;
+    if (len < 3) return true;
+    let key = this.toAbbr(word);
+    if (this.map.has(key)) {
+        let group = this.map.get(key);
+        if (group.length === 1) return group[0] === word;
+        return false;
+    }
+    return true;
+};
+
+ValidWordAbbr.prototype.toAbbr = (word) => {
+    let len = word.length;
+    if (len < 3) return word;
+    return word[0] + (len-2) + word[len-1];
+}
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * var obj = Object.create(ValidWordAbbr).createNew(dictionary)
+ * var param_1 = obj.isUnique(word)
+ */
+
+
+/*
+	=================== solution 2 (96ms) ===================
+*/
+/**
+ * @param {string[]} dictionary is unique
+ */
+var ValidWordAbbr = function(dictionary) {
+    this.map = new Map();
+    for (let i = 0; i < dictionary.length; i++) {
+        let w = dictionary[i];
+        if (w.length < 3) this.map.set(w, true);
+        else {
+            let key = this.toAbbr(w);
+            let val = this.map.has(key) ? false : w;
+            this.map.set(key, val);
+        }
+    }
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+ValidWordAbbr.prototype.isUnique = function(word) {
+    let len = word.length;
+    if (len < 3) return true;
+
+    let key = this.toAbbr(word);
+    if (this.map.has(key)) {
+        let val = this.map.get(key);
+        if (val === false) return false;
+        return word === val;
+    }
+    return true;
+};
+
+ValidWordAbbr.prototype.toAbbr = (word) => {
+    let len = word.length;
+    if (len < 3) return word;
+    return word[0] + (len-2) + word[len-1];
+}
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * var obj = Object.create(ValidWordAbbr).createNew(dictionary)
+ * var param_1 = obj.isUnique(word)
+ */
---
+++ 347__medium__top-k-frequent-elements.js
@@ -0,0 +1,59 @@
+/*
+
+    Given a non-empty array of integers, return the k most frequent elements.
+
+    Example 1:
+
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+    Example 2:
+
+    Input: nums = [1], k = 1
+    Output: [1]
+    Note:
+
+    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let res = [];
+    if (!nums || nums.length === 0) return res;
+
+    // Count frequency of each element use hashmap, O(n) time O(n) space
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+    }
+
+    // new bucket size of n+1, O(n+1) time O(n+1) space
+    let buckets = new Array(nums.length+1).fill(null).map( () => []);
+
+    // push element count into bucket, count will no bigger than n+1 so every element could fit
+    // O(n) time
+    for (let [num, count] of map) {
+        buckets[count].push(num);
+    }
+
+    // O(k) time, find k element from bucket (end to start so we get top k)
+    for (let i = buckets.length-1; i >= 0; i--) {
+        for (let j = 0; j < buckets[i].length; j++) {
+            res.push(buckets[i][j]);
+            if (res.length === k) return res;
+        }
+    }
+};
+/*
+
+    Bucket sort
+    O(n) time
+    O(n) space
+
+*/
